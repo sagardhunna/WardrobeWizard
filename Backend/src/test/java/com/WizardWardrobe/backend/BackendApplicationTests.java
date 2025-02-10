01d7361f31f6e90600ddfb6788503c300c39f8d3
Sagar Dhunna <sagardhunna17@gmail.com>
2025-02-10 12:25:58 -0800

    initial setup commit

---
+++ Backend/src/test/java/com/WizardWardrobe/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WizardWardrobe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
