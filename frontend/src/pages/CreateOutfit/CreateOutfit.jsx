43bc69c22d414df1fdb5b0fbe3928f44b00cf5be
Sagar Dhunna <sagardhunna17@gmail.com>
2025-04-11 23:34:15 -0700

    create outfit page complete, need to work on saving outfits and displaying them on view outfit page

--- frontend/src/pages/CreateOutfit/CreateOutfit.jsx
+++ frontend/src/pages/CreateOutfit/CreateOutfit.jsx
@@ -1,13 +1,148 @@
-import React from 'react'
+import React from "react";
+import "./createOutfit.css";
+import { useState, useEffect } from "react";
 
-function CreateOutfit() {
+const CreateOutfit = () => {
+  const [hat, setHat] = useState(undefined);
+  const [top, setTop] = useState(undefined);
+  const [bottom, setBottom] = useState(undefined);
+  const [shoes, setShoes] = useState(undefined);
+  const [inventory, setInventory] = useState();
+  const [userID, setUserID] = useState();
+  const [outfit, setOutfit] = useState(["", "", "", ""]);
+
+  const [category, setCategory] = useState("hat");
+
+  const setImage = (img) => {
+    if (category === "hat") {
+      setHat(img);
+      outfit[0] = img;
+    } else if (category === "top") {
+      setTop(img);
+      outfit[1] = img;
+    } else if (category === "bottom") {
+      setBottom(img);
+      outfit[2] = img;
+    } else if (category === "shoes") {
+      setShoes(img);
+      outfit[3] = img;
+    }
+  };
+
+  const SERVER = import.meta.env.VITE_SERVER;
+
+  async function getUserID() {
+    try {
+      let options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      };
+      let promise = await fetch(`${SERVER}/getData`, options);
+      let response = await promise.json();
+
+      const userEmail = response.data.userinfo.email;
+
+      options = {
+        method: "POST",
+        body: JSON.stringify({
+          user_email: userEmail,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      promise = await fetch(`${SERVER}/getUserID`, options);
+      response = await promise.json();
+
+      setUserID(response.user_id);
+    } catch (error) {
+      console.log("Error in getUserID", error);
+    }
+  }
+
+  async function getInventory() {
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: userID,
+        }),
+      };
+
+      const promise = await fetch(`${SERVER}/getImages`, options);
+      const response = await promise.json();
+      console.log("Response from getInventory:", response);
+      setInventory(response);
+    } catch (error) {
+      console.log("Error in getInventory:", error);
+    }
+  }
+
+  useEffect(() => {
+    getUserID();
+  }, []);
+
+  useEffect(() => {
+    if (userID != -1) {
+      getInventory();
+      setUserID(-1);
+    }
+  }, [userID]);
+
+
+  const outfitIsComplete = hat && top && bottom && shoes;
+
   return (
-    <div>
-        <h1>
-            Create an outfit here
-        </h1>
-    </div>
-  )
-}
+    <div className="create-outfit-container">
+      <div className="outfit-and-selector">
+        <div className="outfit-creation">
+          {[hat, top, bottom, shoes].map((item, index) => (
+            <div className="outfit-slot" key={index}>
+              <img src={item} alt={`slot-${index}`} draggable="false" />
+            </div>
+          ))}
+          <button className="save-button" onClick={() => {
+            console.log("Outfit:", outfit)
+          }} disabled={!outfitIsComplete}>
+            Save Outfit
+          </button>
+        </div>
+        <div className="clothing-selection">
+          <h2 style={{ color: '#504B38'}}>Select a Category To Place The Image</h2>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="dropdown"
+            style={{ backgroundColor: '#504B38'}}
+          >
+            <option value="hat">Hat</option>
+            <option value="top">Top</option>
+            <option value="bottom">Bottom</option>
+            <option value="shoes">Shoes</option>
+          </select>
 
-export default CreateOutfit
+          <div className="image-gallery">
+            {inventory &&
+              inventory.map((item, i) => (
+                <img
+                  key={i}
+                  src={item.image_url}
+                  alt={`clothing-${i}`}
+                  onClick={() => setImage(item.image_url)}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateOutfit;
